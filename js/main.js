61ee623005534f3de7b415003bf082495522b202
Misaki Koonce <misaki.koonce@gmail.com>
2019-01-31 19:09:36 -0800

    Add mapbox Token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWlzYWtpbWljaHkiLCJhIjoiY2pybGd0bmhhMDhjaTN6cHFoN3BkeWl6ZCJ9.iu9atS7aOf-7j1woNAV60g',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
