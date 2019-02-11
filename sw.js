00bac9ef437da63361b317c52b451e2c90f8c0f4
Misaki Koonce <misaki.koonce@gmail.com>
2019-02-11 11:45:02 -0800

    Update fetch events

--- sw.js
+++ sw.js
@@ -5,17 +5,7 @@ const cacheItem =[
     '/restaurant.html',
     '/css/',
     '/data/',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg',
-    './img/favicon.png',
+    './img/',
     '/js/'
 ];
 
@@ -53,16 +43,15 @@ self.addEventListener('fetch', (event) => {
             if(response) {
                 return response;
             }
-            return fetch(event.request)
-            // .then(response => {
-            //     const responseClone = response.clone();
-            //     caches.open(cacheName).then(cache => {
-            //         cache.put(event.request, responseClone);
-            //     })
-            // }
-            
-        }).catch(error => {
-            console.log('error');
+            return fetch(event.request).then(response => {
+                const responseClone = response.clone();
+                caches.open(assetsCacheName).then(cache => {
+                    cache.put(event.request, responseClone).catch(error => {
+                        console.warn(`request.url: error.message`);
+                    });
+                });
+                return response;
+            });
         })
     );
 });
