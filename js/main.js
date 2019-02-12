c65ae4fdde4fc8833ad8e80e592805f249feb05d
Misaki Koonce <misaki.koonce@gmail.com>
2019-02-11 17:01:37 -0800

    Update alt attribute

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.alt;
+  image.alt = `${restaurant.name} in ${restaurant.neighborhood}`;
   li.append(image);
 
   const name = document.createElement('h1');
